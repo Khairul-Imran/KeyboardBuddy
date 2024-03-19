11af2d04e2761407d958251f5cb26d88f97d847c
Khairul <Khairulimran6810@gmail.com>
2024-03-19 19:08:07 +0800

    Started on word service class.

--- frontend/src/app/testgenerator.service.ts
+++ frontend/src/app/testgenerator.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestgeneratorService {
 
-  
+  private http = inject(HttpClient);
+
+  getRandomWordsTest(testType: string, testDifficulty: string): Observable<any> {
+    const params = new HttpParams()
+      .set('testType', testType)
+      .set('testDifficulty', testDifficulty);
+
+    return this.http.get<any>('/api/words', { params: params });
+  }
+
+
+  getRandomWordsTestLimited(testType: string, testDifficulty: string, limit: number): Promise<any> {
+    const params = new HttpParams()
+      .set('testType', testType)
+      .set('testDifficulty', testDifficulty)
+      .set('limit', limit);
+
+    return lastValueFrom(this.http.get<any>('/api/words', { params: params }));
+  }
 }
