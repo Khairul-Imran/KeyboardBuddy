e35fec9a0615ff13fcb499257f915ab9b7af5706
Khairul <Khairulimran6810@gmail.com>
2024-03-26 23:59:45 +0800

    Settled most of the conditions, to continue tomorrow

--- frontend/src/app/components/typing/typing.component.ts
+++ frontend/src/app/components/typing/typing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
 import { TestgeneratorService } from '../../testgenerator.service';
 import { Observable, Subscription, map } from 'rxjs';
 import { QuicksettingsService } from '../../quicksettings.service';
@@ -15,6 +15,9 @@ export class TypingComponent implements OnInit, OnDestroy {
   private quicksettingsService = inject(QuicksettingsService);
   private subscription!: Subscription;
   // private wordsSubscription!: Subscription;
+
+  @ViewChild('userInputElement') userInputElement!: ElementRef;
+  anyMistakeTracker: boolean = false; // To track if even 1 mistake is made for the word.
   
   // words$!: Observable<Word[]>;
   wordsP$!: Promise<Word[]>;
@@ -23,10 +26,10 @@ export class TypingComponent implements OnInit, OnDestroy {
   currentLetterIndex: number = 0;
   currentWordIndex: number = 0;
 
-  testType: string = "time";
-  testDifficulty: string = "easy";
+  testType!: string;
+  testDifficulty!: string;
   testWordLimit!: number;
-  testDuration: number = 30;
+  testDuration!: number;
 
   ngOnInit(): void {
     // Receives updated settings from testSettings$ Observable.
@@ -58,7 +61,9 @@ export class TypingComponent implements OnInit, OnDestroy {
       console.log(`Settings: Type=${this.testType}, Difficulty=${this.testDifficulty}`);
       this.wordsP$ = this.testgeneratorService.getRandomWordsTest(this.testType, this.testDifficulty)
         .then(words => {
+          console.info("Words to be inserted: ", words);
           this.wordsFromPromise = words;
+          console.info("Words in the array: ", this.wordsFromPromise);
           return this.wordsFromPromise;
         })
         .catch(error => {
@@ -69,14 +74,16 @@ export class TypingComponent implements OnInit, OnDestroy {
       // this.wordsSubscription = this.words$.subscribe(words => {
       //   this.wordsFromObservable = words;
       // })
-      console.info("These are the words inside the array: ", this.wordsFromPromise);
+      // console.info("These are the words inside the array: ", this.wordsFromPromise);
 
     } else if (this.testType === 'words') {
       // Word-based
       console.log(`Settings: Type=${this.testType}, Difficulty=${this.testDifficulty}, Word Limit=${this.testWordLimit}`);
       this.wordsP$ = this.testgeneratorService.getRandomWordsTestLimited(this.testType, this.testDifficulty, this.testWordLimit)
         .then(words => {
+          console.info("Words to be inserted: ", words);
           this.wordsFromPromise = words;
+          console.info("Words in the array: ", this.wordsFromPromise);
           return this.wordsFromPromise;
         })
         .catch(error => {
@@ -87,8 +94,13 @@ export class TypingComponent implements OnInit, OnDestroy {
       // this.wordsSubscription = this.words$.subscribe(words => {
       //   this.wordsFromObservable = words;
       // })
-      console.info("These are the words inside the array: ", this.wordsFromPromise);
+      // console.info("These are the words inside the array: ", this.wordsFromPromise);
     }
+
+    // Reset the indexes after generating each test.
+    this.currentLetterIndex = 0;
+    this.currentWordIndex = 0;
+    this.userInputElement.nativeElement.focus(); // Sets the focus into the input element.
   }
 
   onUserInput(event: Event) {
@@ -96,6 +108,8 @@ export class TypingComponent implements OnInit, OnDestroy {
     const currentWord: Word = this.wordsFromPromise[this.currentWordIndex];
     const currentLetter: Letter = currentWord.letters[this.currentLetterIndex];
 
+    console.info("Current word: ", currentWord);
+    console.info("Current letter: ", currentLetter);
 
     // Recall that in the array, the letters are all back-to-back (no spaces)
     // This is different from how the css presents them.
@@ -104,17 +118,30 @@ export class TypingComponent implements OnInit, OnDestroy {
       currentLetter.correct = true;
       this.currentLetterIndex++; // Move to the next letter.
 
-      if (this.currentLetterIndex === currentWord.letters.length) { // If all the letters are correct | currentLetterIndex is correct?
-        currentWord.fullyCorrect = true;
-        this.currentWordIndex++;
-        this.currentLetterIndex = 0;
-      }
-    } else {
+      // if (this.currentLetterIndex === currentWord.letters.length) { // If all the letters are correct | currentLetterIndex is correct?
+      //   currentWord.fullyCorrect = true;
+      //   this.currentWordIndex++;
+      //   this.currentLetterIndex = 0;
+      // }
+    } else { // If wrong letter
       currentLetter.correct = false;
       currentLetter.untouched = false;
+      this.anyMistakeTracker = true;
+      this.currentLetterIndex++;
     }
 
-    // (event.target as HTMLInputElement).value = '';
+    // Goes to the next word after you've reached the last letter.
+    if (this.currentLetterIndex === currentWord.letters.length) {
+      if (!currentWord.fullyCorrect && this.anyMistakeTracker) {
+        currentWord.untouched = false;
+      }
+      
+      this.currentWordIndex++; // Go to the next word.
+      this.currentLetterIndex = 0; // Go back to the first letter.
+      this.anyMistakeTracker = false;
+    }
+
+    (event.target as HTMLInputElement).value = '';
 
   }
 
